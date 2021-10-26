3c966b7b2b85c43f7ebd91a9d2b5699953b0b2cb
JP Beveraggi <korchov@shannon.knet>
2021-10-26 11:21:01 +0100

    Exported get_mode_names function

--- src/lib.rs
+++ src/lib.rs
@@ -7,5 +7,5 @@ mod chord;
 pub use note::{Note, NoteAccidental, NoteLetter, next_note_letter};
 pub use midinote::MIDINote;
 pub use chord::{ChordQuality, Chord, ChordPosition};
-pub use mode::{Scale, Mode};
+pub use mode::{Scale, Mode, get_mode_names};
 pub use interval::Interval;
